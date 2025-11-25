eb32ecf3a2112ee4c2306a2ec667b32e92aebb02
nrbnayon <nrbnayon@gmail.com>
2025-11-25 14:25:03 +0600

    size check

---
+++ dataTypeCheck.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter the size of sorted array: ";
+    cin >> n;
+    cout << "int: " << n << " (size: " << sizeof(n) << " bytes)\n";
+}
